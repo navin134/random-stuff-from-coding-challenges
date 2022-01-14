94ac838654ae6e360509ecbd08b7cb6dd035cc4c
Fynn M M Gallup <89165718+navin134@users.noreply.github.com>
2022-01-14 08:30:14 -0800

    Create ForceCalculations.java

---
+++ ForceCalculations.java
@@ -0,0 +1,44 @@
+public class ForceCalculations {
+    private int yForceVector;
+    private int xForceVector;
+    private int summedAngle;
+    private int forceOfairfoils;
+    private int forceOfMIP;
+    
+    public ForceCalculations() {
+        this.yForceVector = 0;
+        this.xForceVector = 0;
+        this.summedAngle = 0;
+        this.forceOfairfoils = 0;
+        this.forceOfMIP = 0;
+        
+    }
+    
+    public void getYForceVector() {
+        return this.yForceVector;
+        
+    }
+    
+    public void getXForceVector() {
+        return this.xForceVector;
+        
+    }
+    
+    public void getSummedAngle() {
+        return this.summedAngle;
+        
+    }
+    
+    public void getForceOfairfoils() {
+        return this.forceOfairfoils;
+        
+    }
+    
+    public void getForceOfMIP() {
+        return this.forceOfMIP;
+        
+    }
+    
+    
+    
+}  
